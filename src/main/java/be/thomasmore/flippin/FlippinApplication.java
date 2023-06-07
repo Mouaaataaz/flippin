3e6a06e91ccb4bd1bf2fb6bcebb1d78f98d7cfdf
mouaataaz <r0956108@student.thomasmore.be>
2023-06-07 15:30:32 +0200

    Initial commit

---
+++ src/main/java/be/thomasmore/flippin/FlippinApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.flippin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlippinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlippinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/thomasmore/flippin/FlippinApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.flippin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlippinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
