540efd16669d1416204bd608f9e7fb1ff72a5b29
mouaataaz <r0956108@student.thomasmore.be>
2023-06-08 16:21:49 +0200

    About page added

--- src/main/java/be/thomasmore/flippin/controllers/HomeController.java
+++ src/main/java/be/thomasmore/flippin/controllers/HomeController.java
@@ -1,12 +1,17 @@
 package be.thomasmore.flippin.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping({"/", "/home"})
     public String home() {
         return "home";
+    }
+    @GetMapping("/about")
+    public String about(Model model) {
+        return "about";
     }
 }
