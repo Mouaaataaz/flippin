4c7c2afa285f84033668cfe319ee80ce5275ddaf
mouaataaz <r0956108@student.thomasmore.be>
2023-06-10 16:09:09 +0200

    ecommercedetail page layout update

--- src/main/java/be/thomasmore/flippin/controllers/EcommerceController.java
+++ src/main/java/be/thomasmore/flippin/controllers/EcommerceController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Optional;
 
@@ -24,5 +25,15 @@ public class EcommerceController {
         return "ecommercelist";
     }
 
+    @GetMapping({"/ecommercedetails", "/ecommercedetails/{id}"})
+    public String ecommerceDetails(Model model, @PathVariable(required = false) Integer id) {
+        if (id == null) return "artistdetails";
+        Optional<Ecommerce> optionalEcommerce = ecommerceRepository.findById(id);
+
+        if (optionalEcommerce.isPresent()) {
+            model.addAttribute("ecommercelisting", optionalEcommerce.get());
+        }
+        return "ecommercedetails";
+    }
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,17 @@ insert into ECOMMERCE (
                        PAYMENT_METHOD, LOCATION)
 values (
            1, 'Pikiki', 'FBA', 15000, 500, '2015', 'Good', 'Paypal, CreditCard', 'USA');
+
+insert into ECOMMERCE (
+    ID, ECOMMERCE_NAME, TYPE, PRICE, MONTHLY_PROFIT, FOUNDATION, PRODUCTS_RATINGS,
+    PAYMENT_METHOD, LOCATION)
+values (
+           2, 'Bilaa', 'KDP', 25000, 1200, '2013', 'Great', 'CreditCard', 'CANADA');
+
+insert into ECOMMERCE (
+    ID, ECOMMERCE_NAME, TYPE, PRICE, MONTHLY_PROFIT, FOUNDATION, PRODUCTS_RATINGS,
+    PAYMENT_METHOD, LOCATION)
+values (
+           3, 'Bruh', 'MBA', 25700, 1210, '2017', 'Perfect', 'CreditCart', 'BELGIUM');
+
+
